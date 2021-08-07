5cb63c23f47a2e0ce5103c76c04d77de9fdb153f
Yolan Maldonado <register@yolan.dev>
2021-08-07 17:08:52 +0200

    :(

--- routes/price.js
+++ routes/price.js
@@ -5,7 +5,7 @@ const percent_diff = require('percentage-difference');
 
 const getFormattedData = async (invest) => {
     const price = await getPrice();
-    const actualValue = (price * invest) * 0.78
+    const actualValue = (price * invest) * 0.20
     const percent = Math.round(percent_diff(invest, actualValue))
     const amount = Math.round(actualValue)
     return {amount, percent}
